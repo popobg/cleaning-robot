2674c3fb521a5c20a3419f2ea22bab6be53831e6
popobg <pauline.bouyssou1@gmail.com>
2024-12-13 17:10:45 +0100

    Console app fonctionnelle

--- console_app/utilities/affichergrille.js
+++ console_app/utilities/affichergrille.js
@@ -1,19 +1,23 @@
 class AfficherGrille {
-    constructor(grille) {
-        let grille = this.grille;
+    constructor() {
     }
-    afficher(grille, rx,ry)  
+
+    Afficher(grille, rx,ry)
     {
-        console.clear();
-        this.grille.forEach((ligne, y) => {
+        console.log("=========================");
+        console.log();
+
+        // console.clear();
+        grille.forEach((ligne, y) => {
         const ligneAffichee = ligne.map((caseGrille, x) =>
         x === rx && y === ry ? "R" : caseGrille
             );
         console.log("|" + ligneAffichee.join("|") + "|");
         });
+
+        console.log();
+        console.log("=========================");
     }
- 
- 
 }
-                
- 
+
+module.exports = AfficherGrille;
