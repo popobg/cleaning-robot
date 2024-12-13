5f2d02c3820f803e4bd1b9575e179cf221249d62
juliagrandmontagne <j.gmt@outlook.fr>
2024-12-13 13:23:09 +0100

    Update grille.js

--- console_app/entites/grille.js
+++ console_app/entites/grille.js
@@ -1,34 +1,40 @@
 class Grille {
-    constructor(largeur = 5, hauteur = 5, poussiere = 25) {
+    constructor(largeur = 5, hauteur = 5, pourcentagepoussiere = 25) {
         this.largeur = largeur; 
         this.hauteur = hauteur; 
         this.pourcentagepoussiere = this.pourcentagepoussiere;
         this.CreateGrid=this.CreateGrid;
     }
     CreateGrid()  { 
-        let grille = [];
+        let oGrille = [];
         for (let y = 0; y < this.hauteur; y++)  { 
             const ligne = [];
             for (let x = 0; x < this.largeur; x++)  
             {
                 ligne.push(Math.random() < this.pourcentagepoussiere / 100 ? " " : "X");
              }
-             grille.push(ligne);
+             oGrille.push(ligne);
         }
-        return grille;
+        return oGrille;
     }
-    getheight() {
+    UpdateGrid(x, y){ 
+        if (this.oGrille?.[y]?.[x] === " ") { 
+            this.oGrille[y][x] = "X";
+        }
+    }
+    getlargeur() {
         return this.largeur;
     }
-    getwidth() {
+    gethauteur() {
         return this.hauteur;
     }
 
-    setheight(largeur) {
+    setlargeur(largeur) {
         this.x = largeur;
     }
 
-    setwidth(hauteur) {
+    sethauteur(hauteur) {
         this.hauteur = hauteur;
     }
 }
+const grille = new Grille(largeur, hauteur, poussiere);
