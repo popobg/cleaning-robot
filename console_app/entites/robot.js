f3db01a15362d87e41c56ceb6a6e508de486a0db
popobg <pauline.bouyssou1@gmail.com>
2024-12-15 22:31:24 +0100

    Suppression code mort

--- console_app/entites/robot.js
+++ console_app/entites/robot.js
@@ -40,48 +40,6 @@ class Robot {
         console.log(`Le robot se déplace vers ${direction} de la case [${anciennePosition.GetX()}, ${anciennePosition.GetY()}] vers la case [${nouvellePosition.GetX()}, ${nouvellePosition.GetY()}].`);
     }
 
-    // Appelée par la fonction logique métier
-    // Déplace le robot de gauche à droite et bas en haut
-    SeDeplacer(oGrille) {
-        const largeurMax = oGrille.GetLargeur() - 1;
-        const hauteurMax = oGrille.GetHauteur() - 1;
-
-        // Bout de la grille atteint
-        if (this.position.GetX() === largeurMax && this.position.GetY() === hauteurMax) {
-            return;
-        }
-
-        // premier déplacement, robot en (0,0)
-        if (this.historiquePosition.length === 1) {
-            this.SetPosition(this.position.GetX() + 1, this.position.GetY());
-        }
-        else {
-            const dernierePosition = this.historiquePosition[this.historiquePosition.length - 2];
-
-            // Le robot arrive en bout de ligne parcourue
-            if ((this.position.GetX() === largeurMax || this.position.GetX() === 0)
-                && dernierePosition.GetY() === this.position.GetY()) {
-                    // Le robot se déplace vers le bas
-                    this.SetPosition(this.position.GetX(), this.position.GetY() + 1);
-                    // Inverse la direction de déplacement
-                    this.avancer = !this.avancer;
-            }
-            else {
-                if (this.avancer) {
-                    // le robot se déplace vers la droite
-                    this.SetPosition(this.position.GetX() + 1, this.position.GetY());
-                }
-                else {
-                    // le robot se déplace vers la gauche
-                    this.SetPosition(this.position.GetX() - 1, this.position.GetY());
-                }
-            }
-        }
-
-        this.AjouterPositionHistorique();
-        this.AfficherDeplacement();
-    }
-
     SeDeplacerIntelligemment(casesSales) {
         if (this.objectif.x === this.position.GetX()
             && this.objectif.y === this.position.GetY()) {
